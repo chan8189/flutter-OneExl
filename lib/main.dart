3ad4a19131fe8dd83e0611b531314855b8a565bb
Chandan Choudhary <chaudhary.chandan48@gmail.com>
2020-03-30 03:38:44 +0530

    splash_screen initial commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_app/homeScreen.dart';
 
 void main(){
-  runApp(
-    Center(
-        child: Text(
-      "Hello Chandan",
-      textDirection: TextDirection.ltr,
-    ),
-    )
-  );
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+    routes: <String, WidgetBuilder>{
+      '/HomeScreen': (BuildContext context) => new HomeScreen()
+    },
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Center(
+        child: new Image.asset('images/splash_screen.png'),
+      ),
+    );
+  }
 }

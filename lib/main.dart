04d576e8a30cc7a7a218e523ef3d9b4ff957e640
Chandan Choudhary <chaudhary.chandan48@gmail.com>
2020-03-30 02:54:31 +0530

    initial project setup commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    Center(
+        child: Text(
+      "Hello Chandan",
+      textDirection: TextDirection.ltr,
+    ),
+    )
+  );
+}
